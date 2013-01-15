638d078e21d8bd5212eaf42b5ae1593c1a08948c
Paul W. <pwoidke@gmail.com>
2013-01-14 22:42:30 -0500

    Added support for multiple artists & album images
    Program now uses first artist if there are more than 1 (this should
    probably be changed to a "x / y / z" format in the future), and uses the
    first image if there is more than one (should add check for "primary"
    image). Also, the Discogs API locks you out after 1000 requests/24
    hours/IP address. That's kinda lame. I will have to explore workarounds.

--- js/main.js
+++ js/main.js
@@ -19,17 +19,34 @@ records = X2JS.xml_str2json( xmlDoc );
 
 for(var i=0;i<records.releases.release.length;i++)
 {
-    $('body').append('<div class="record" id="' + records.releases.release[i]._id + '"><div class="artist">' + records.releases.release[i].artists.artist.name + '</div><div class="title">' + records.releases.release[i].title + '</div></div>');
     try
     {
-        $('.record:last').css('background-image','url("' + records.releases.release[i].images.image._uri150 + '")');
-        $('.record:last').click(function() {
-            alert("Handler called for #" + this.id);
-        });
+        if(typeof records.releases.release[i].artists.artist[0] === 'undefined')
+        {
+            $('body').append('<div class="record" id="' + records.releases.release[i]._id + '"><div class="artist">' + records.releases.release[i].artists.artist.name + '</div><div class="title">' + records.releases.release[i].title + '</div></div>');
+        }
+        else
+        {
+            $('body').append('<div class="record" id="' + records.releases.release[i]._id + '"><div class="artist">' + records.releases.release[i].artists.artist[0].name + '</div><div class="title">' + records.releases.release[i].title + '</div></div>');
+        }
+        if(typeof records.releases.release[i].images.image[0]._uri150 === 'undefined')
+        {
+            $('.record:last').css('background-image','url("' + records.releases.release[i].images.image._uri150 + '")');
+        }
+        else
+        {
+            $('.record:last').css('background-image','url("' + records.releases.release[i].images.image[0]._uri150 + '")');
+        }
     }
     catch(err)
     {
         console.log(err.message);
+    }
+    finally
+    {
+        $('.record:last').click(function() {
+            alert("Handler called for #" + this.id);
+        });
     }
 }
 

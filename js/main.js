18f9aa3965211dc10b1b1de1a2e29b5691842534
Paul W. <pwoidke@gmail.com>
2013-01-15 17:08:00 -0500

    Made sizes change dynamically
    I was getting tired of calculating and changing a million variables
    every time I decided the spines were too short, so I went ahead and
    coded some jQuery that sets the spine height (width), margins, offsets,
    etc. from a single variable ('spineWidth' at ln. 63 of main.js). I
    should probably do the same thing for heights, but I like them how they
    are right now so no big. Also, I fixed the dynamically generated width
    of the records div.

--- js/main.js
+++ js/main.js
@@ -12,13 +12,14 @@ xmlDoc=xmlhttp.responseText;
 var X2JS = new X2JS();
 records = X2JS.xml_str2json( xmlDoc );
 
+//alert(records.releases.__cnt);
 
 function getRandomColor()
 {
     return 'rgb(' + (Math.floor((200)*Math.random())) + ',' + (Math.floor((200)*Math.random())) + ',' + (Math.floor((200)*Math.random())) + ')';
 }
 
-$('#records').css("width", ((records.releases.release.length * 10) + 20) + "px");
+$('#records').css("width", ((records.releases.release.length * 23) + 10) + "px");
 
 for(var i=0;i<records.releases.release.length;i++)
 {
@@ -35,7 +36,6 @@ for(var i=0;i<records.releases.release.length;i++)
         }
 
         /* Set background color */
-//        $('.spine:last').css('background-color','red');
         $('.spine:last').css('background-color',getRandomColor());
 
         /* Add album covers */
@@ -60,13 +60,19 @@ for(var i=0;i<records.releases.release.length;i++)
     }
 }
 
+var spineWidth = 600;
+
+$("#shelf").css("height", (spineWidth+"px"));
+$("#records").css("height", (spineWidth+"px"));
+$(".spine").css({width: (spineWidth+"px"), top: (((spineWidth-20)/2)+"px"), right: (((spineWidth-20)/2)+"px"), marginBottom: (spineWidth+"px"), marginRight: ((-1*(spineWidth-23))+"px")});
+
 /* Expand album on mouseover */
 $(".spine").hover(function(){
-    $(this).stop(true, false).animate({height: "30px", marginRight: "-230px", marginLeft: "10px", marginBottom: "240px", marginTop: "-10px"});
-    $(this).children().stop(true, false).animate({fontSize: "20px", top: "0px" });
+    $(this).stop(true, false).animate({height: "30px",  width: ((spineWidth+10)+"px"), marginTop: "-10px"}); /* new width = old width + 10 */
+    $(this).children().stop(true, false).animate({fontSize: "20px", top: "0px", marginLeft: "20px" });
 }, function() {
-    $(this).stop(true, false).animate({height: "10px", marginRight: "-240px", marginLeft: "0px", marginBottom: "250px", marginTop: "0px"});
-    $(this).children().stop(true, false).animate({fontSize: "10px", top: "-10px" });
+    $(this).stop(true, false).animate({height: "20px", width: (spineWidth+"px"), marginTop: "0px"});
+    $(this).children().stop(true, false).animate({fontSize: "15px", top: "-3px", marginLeft: "5px" });
 });
 
 /* Scroll records on mouseover on Left/Right buttons */

cd262f99374ed499dacca61f4f72f20bbf426f59
Paul W. <pwoidke@gmail.com>
2013-01-15 11:13:15 -0500

    Changed covers to spines (+ colors!)
    Since the Discogs API won't allow me to load the covers of everything
    (there is a 1000 request/day limit), I changed the 'records' to
    'spines'. They now say the artist & title, and are arranged as little
    narrow bars (like record spines) which are colored randomly via a JS
    function. Added jQuery to increase size on mouseover. Next up, make it
    all in one row, add scrolling functionality, and display extended
    information on click.

--- js/main.js
+++ js/main.js
@@ -10,36 +10,42 @@ xmlhttp.open("GET","data/vinyl.xml",false);
 xmlhttp.send();
 xmlDoc=xmlhttp.responseText;
 var X2JS = new X2JS();
-// XML string to JSON
-
 records = X2JS.xml_str2json( xmlDoc );
 
-//alert(records.releases.release[0]._id);
 
+function getRandomColor()
+{
+    var hue = 'rgb(' + (Math.floor((200)*Math.random())) + ',' + (Math.floor((200)*Math.random())) + ',' + (Math.floor((200)*Math.random())) + ')';
+    return hue;
+}
 
 for(var i=0;i<records.releases.release.length;i++)
 {
     try
     {
+        /* Add Artist & Title */
         if(typeof records.releases.release[i].artists.artist[0] === 'undefined')
         {
-            $('body').append('<div class="record" id="' + records.releases.release[i]._id + '"><div class="artist">' + records.releases.release[i].artists.artist.name + '</div><div class="title">' + records.releases.release[i].title + '</div></div>');
+            $('#records').append('<div class="spine" id="' + records.releases.release[i]._id + '"><div class="artist-title">' + records.releases.release[i].artists.artist.name + ' - ' + records.releases.release[i].title + '</div></div>');
         }
         else
         {
-            $('body').append('<div class="record" id="' + records.releases.release[i]._id + '"><div class="artist">' + records.releases.release[i].artists.artist[0].name + '</div><div class="title">' + records.releases.release[i].title + '</div></div>');
+            $('#records').append('<div class="spine" id="' + records.releases.release[i]._id + '"><div class="artist-title">' + records.releases.release[i].artists.artist[0].name + ' - ' + records.releases.release[i].title + '</div></div>');
         }
 
-//        alert(records.releases.release[i].images.image._uri150);
+        /* Set background color */
+//        $('.spine:last').css('background-color','red');
+        $('.spine:last').css('background-color',getRandomColor());
 
-        if(records.releases.release[i].images.image._uri150)
+        /* Add album covers */
+        /*if(records.releases.release[i].images.image._uri150)
         {
             $('.record:last').css('background-image','url("' + records.releases.release[i].images.image._uri150 + '")');
         }
         else
         {
             $('.record:last').css('background-image','url("' + records.releases.release[i].images.image[0]._uri150 + '")');
-        }
+        }*/
     }
     catch(err)
     {
@@ -47,10 +53,18 @@ for(var i=0;i<records.releases.release.length;i++)
     }
     finally
     {
-        $('.record:last').click(function() {
+        $('.spine:last').click(function() {
             alert("Handler called for #" + this.id);
         });
     }
 }
 
-//TODO: Check artist info for RHCP - Havana Affair
+/* Expand album on mouseover */
+$(".spine").hover(function(){
+    $(this).stop(true, false).animate({ height: "30px", marginRight: "-230px", marginLeft: "10px", marginBottom: "240px"});
+    $(this).children().stop(true, false).animate({ fontSize: "20px", top: "0px" });
+
+}, function() {
+    $(this).stop(true, false).animate({ height: "10px", marginRight: "-240px", marginLeft: "0px", marginBottom: "250px"});
+    $(this).children().stop(true, false).animate({ fontSize: "10px", top: "-10px" });
+});

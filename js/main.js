9b2066489b46447b85d6ef66178bbb4dd4fcce83
Paul W. <pwoidke@gmail.com>
2013-04-08 22:55:07 -0400

    Rebuilding for cross-browser compatibility
    Since it's taking a long time and a lot of effort to get things working
    with Webkit browsers, I've decided to just take everything down to the
    bare minimum and rebuild. This commit just has the header/footer, list,
    and info showing on click.

--- js/main.js
+++ js/main.js
@@ -12,7 +12,9 @@ var spineWidth = 600;
 var spineHeight = 20;
 
 
-var records;
+var records, infoShown;
+
+infoShown = false;
 getData();
 
 
@@ -35,7 +37,6 @@ function getData()
     records = x2js.xml_str2json( xmlDoc );
 
     buildRecords(records.releases.release.length);
-    buildUI(spineWidth, spineHeight, records.releases.release.length);
 }
 
 
@@ -58,9 +59,6 @@ function buildRecords(length)
             {
                 $('.records').append('<li class="spine" id="' + i + '"><div class="artist-title">' + records.releases.release[i].artists.artist[0].name + ' - ' + records.releases.release[i].title + '</div></li>');
             }
-
-            /* Set background color */
-            $('.spine:last').css('background-color',getRandomColor());
         }
         catch(err)
         {
@@ -69,191 +67,22 @@ function buildRecords(length)
     }
 }
 
-function buildUI(spineWidth, spineHeight, length)
-{
-    /* Style UI elements based on width/height of spines */
-    $('.spine').css({
-        width: (spineWidth+"px"),
-        height: (spineHeight+"px"),
-        top: (((spineWidth-spineHeight)/2)+"px"),               //FIREFOX
-        //top: ((-1*(((spineWidth+spineHeight)/2)+3))+"px"),    //CHROME TODO: fix this
-        right: (((spineWidth-spineHeight)/2)+"px"),
-        marginBottom: (spineWidth+"px"),
-        marginRight: ((-1*(spineWidth-(spineHeight+3)))+"px")
-    }).children().css({
-        fontSize: (0.75*spineHeight+"px"),
-        marginLeft: (0.25*spineHeight+"px")
-    });
-    $('.shelf').css({
-        height:(spineWidth+"px")
-    });
-    $('.records').css({
-        width:(((length * 23) + 10) + "px"),
-        height:(spineWidth+"px")
-    });
-    $('.scroll-left, .scroll-right').css({
-        height: (spineWidth+"px"),
-        width: ((spineWidth *0.08)+"px")
-    });
-}
-
-
-/* EVENTS */
-
-var infoShown = false;
-var tracksShown = false;
-var videosShown = false;
-
-$('.spine').click(function(e) {
-    //Shift records so that spine is centered on shelf
-    $('.records').animate({
-        left: (parseInt($('.records').css("left"), 10)                  /* Get the amount the records div is shifted left */
-            + (($('.shelf').width()/2)                                  /* Add the xPos of the center of the shelf div */
-            - (e.clientX - $('.shelf').offset().left))                  /* Minus the xPos of the click from the center to get the amount to shift */
-            - ((spineHeight/2) - (e.clientX - $(this).offset().left)))  /* Align center of spine with center of shelf (use unexpanded height because when it moves it won't have the mouseover on it anymore) */
-    });
-
-    if(infoShown == false)
+$('.spine').click(function(){
+    loadInfo(this.id);
+    if(!infoShown)
     {
-        $('.container').animate({top:"25px"}, 500);
-        $('.info').animate({opacity:"1.0"}, 500, function() {
-            $('.tracklist').animate({opacity:"1.0", marginLeft:"140px"}, 500);
-            $('.videolist').animate({opacity:"1.0", marginLeft:"-30px"}, 500);
-        });
+        $('.info').show();
         infoShown = true;
-        loadInfo(this.id);
     }
-    else
+});
+
+$('.info').click(function(){
+    if(infoShown)
     {
-        loadInfo(this.id);
-    }
-});
-
-$('.info').click(function () {                      //TODO: disable everything when hidden (image still links)
-    $('.container').animate({top:"-200px"}, 500);
-    $('.info').animate({opacity:"0.0"}, 500);
-    $('.tracklist').animate({opacity:"0.0", marginLeft:"80px"}, 100);
-    $('.videolist').animate({opacity:"0.0", marginLeft:"10px"}, 100);
-    infoShown = false;
-});
-
-$('.tracklist').click(function() {
-    if(tracksShown)
-    {
-        $('.tracklist').animate({marginLeft:"140px"}, 500);
-        tracksShown = false;
-    }
-    else
-    {
-        $('.tracklist').animate({marginLeft:"390px"}, 500);
-        tracksShown = true;
-    }
-});
-
-$('.videolist').click(function() {
-    if(videosShown)
-    {
-        $('.videolist').animate({marginLeft:"-30px"}, 500);
-        videosShown = false;
-    }
-    else
-    {
-        $('.videolist').animate({marginLeft:"-280px"}, 500);
-        videosShown = true;
-    }
-});
-
-/* Expand album on mouseover */
-$(".spine").hover(function(){
-    $(this).stop(true, false).animate({
-        height: (1.5*spineHeight+"px"),
-        top: (((spineWidth-spineHeight)/2)+(0.25*spineHeight)+"px"),
-        right: (((spineWidth-spineHeight)/2)-(0.25*spineHeight)+"px"),
-        marginRight: ((-1*(spineWidth-(spineHeight+3)))+(0.5*spineHeight)+"px"),
-        marginTop: ((-1*spineHeight)+"px")
-    });
-    $(this).children().stop(true, false).animate({
-        fontSize: (spineHeight+"px"),
-        top: "0px",
-        marginLeft: (0.5*spineHeight+"px")
-    });
-}, function() {
-    $(this).stop(true, false).animate({
-        height: (spineHeight+"px"),
-        top: (((spineWidth-spineHeight)/2)+"px"),
-        right: (((spineWidth-spineHeight)/2)+"px"),
-        marginRight: ((-1*(spineWidth-(spineHeight+3)))+"px"),
-        marginTop:"0px"
-    });
-    $(this).children().stop(true, false).animate({
-        fontSize: (0.75*spineHeight+"px"),
-        top: "-3px",
-        marginLeft: (0.25*spineHeight+"px")
-    });
-});
-
-/* Scroll records on mouseover on Left/Right buttons */
-var scrolling = false;
-
-$('.scroll-left').bind("mouseover", function(event) {
-    scrolling = true;
-    scrollContent("left");
-}).bind("mouseout", function(event) {
-    scrolling = false;
-});
-
-$('.scroll-right').bind("mouseover", function(event) {
-        scrolling = true;
-        scrollContent("right");
-}).bind("mouseout", function(event) {
-    scrolling = false;
-});
-
-$(document).keydown(function (e) {
-    e = e || window.event;
-
-    if (e.keyCode == '37') {
-        scrollContent("left");
-    }
-    else if (e.keyCode == '39') {
-        scrollContent("right");
-    }
-    else if (e.keyCode == '13') {
-        var centerID = (((-1 * (parseInt($('.records').css("left"), 10) - ($('.shelf').width() / 2))) - 18) / 23);
-        if (infoShown) {
-            if (centerID < 0) {
-                loadInfo(0);
-            }
-            else {
-                loadInfo(1 + (parseInt(centerID, 10)));
-            }
-        }
-        else {
-            $('.container').animate({top: "25px"}, 500);
-            $('.info').animate({opacity: "1.0"}, 500, function () {
-                $('.tracklist').animate({opacity: "1.0", marginLeft: "140px"}, 500);
-                $('.videolist').animate({opacity: "1.0", marginLeft: "-30px"}, 500);
-            });
-            infoShown = true;
-            if (centerID < 0) {
-                loadInfo(0);
-            }
-            else {
-                loadInfo(1 + (parseInt(centerID, 10)));
-            }
-        }
-    }
-    else if ((e.keyCode == '27') && infoShown) {
-        $('.container').animate({top: "-200px"}, 500);
-        $('.info').animate({opacity: "0.0"}, 500);
-        $('.tracklist').animate({opacity: "0.0", marginLeft: "80px"}, 100);
-        $('.videolist').animate({opacity: "0.0", marginLeft: "10px"}, 100);
+        $('.info').hide();
         infoShown = false;
     }
 });
-
-
-/* EVENT FUNCTIONS */
 
 function loadInfo(id)
 {
@@ -377,7 +206,7 @@ function loadInfo(id)
                 $('ul.tracksUL').append(" (" + trackDuration + ")");
             }
             $('ul.tracksUL').append("</li>");
-        }
+       }
     }
     $('ul.videosUL').text('');
     if(!(typeof records.releases.release[id].videos === 'undefined'))
@@ -398,7 +227,7 @@ function loadInfo(id)
                 videoTitle = htmlEncode(records.releases.release[id].videos.video.title);
             }
             else
-            {
+           {
                 videoTitle = htmlEncode(records.releases.release[id].videos.video[i].title);
             }
 
@@ -411,21 +240,6 @@ function loadInfo(id)
     }
 
     makePretty();
-}
-
-function scrollContent(direction)
-{
-    if(((direction==="left")&&(parseInt($(".records").css("left"),10)<((parseInt($(".shelf").css("width"),10)/2)-10)))||((direction==="right")&&(parseInt($(".records").css("left"),10)>(-1*(((parseInt($(".records").css("width"),10))-23)-((parseInt($(".shelf").css("width"),10))/2))))))
-    {
-        var amount = (direction === "right" ? "-=2px" : "+=2px");
-        $('.records').animate({
-            left: amount
-        }, 1, function() {
-            if (scrolling) {
-                scrollContent(direction);
-            }
-        });
-    }
 }
 
 
